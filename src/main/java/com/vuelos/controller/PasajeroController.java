dd2ade2b379c1cd415268f000ca9848db23f44ea
karen <karenrojas0419@gmail.com>
2020-11-16 16:19:30 -0500

    Elimina pasajeros

--- src/main/java/com/vuelos/controller/PasajeroController.java
+++ src/main/java/com/vuelos/controller/PasajeroController.java
@@ -3,6 +3,7 @@ package com.vuelos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,10 @@ public class PasajeroController {
 		return repo.findById(id);
 	}
 	
+	@DeleteMapping("/eliminar/{id}")
+	public void eliminarPasajero(@PathVariable("id") Integer id) {
+		repo.deleteById(id);
+	}
 	
 	
 	

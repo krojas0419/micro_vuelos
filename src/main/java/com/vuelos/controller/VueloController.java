a70792b81fcd80aeb94fca21f79c65f696316fcd
karen <karenrojas0419@gmail.com>
2020-11-24 21:03:26 -0500

    Agrego CrossOrigin al controlador

--- src/main/java/com/vuelos/controller/VueloController.java
+++ src/main/java/com/vuelos/controller/VueloController.java
@@ -3,13 +3,14 @@ package com.vuelos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vuelos.model.Vuelo;
 import com.vuelos.service.VueloService;
 
-
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 public class VueloController {
 
